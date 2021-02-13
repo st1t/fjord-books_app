4221e5102f4c9e440aae5c90be5191ae47002974
Shota Ito <shota.ito.jp@gmail.com>
2021-02-14 01:57:59 +0900

    Add homes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:"books#index"
+  root to:"homes#index"
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
