a7f711021f333aadd236583298e382c46f9b740c
Shota Ito <shota.ito.jp@gmail.com>
2021-02-14 23:25:47 +0900

    Modify application_controller.rb to customize strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address#strong-parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:postal_code, :address, :self_introduction, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
