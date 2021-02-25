34dfa4f3a10dc223c208d8968fa657545ab0ff56
Shota Ito <shota.ito.jp@gmail.com>
2021-02-25 19:20:57 +0900

    Modify search method from find_by to find in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    u = User.find_by(id: params[:id])
-    if u.nil?
-      # TO DO 本来は404ページを用意してリダイレクトの方が良さそう
-      redirect_to '/users'
-    else
-      @user = User.find_by(id: params[:id])
-    end
+    @user = User.find(params[:id])
   end
 end
